5347afc95273cfe96c176b8fdfa6f0265b3f6b3d
pavanbharathbalasani <pavanbharathbalasani@gmail.com>
2024-06-26 23:34:45 +0530

    fixed styling and add new features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styled from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
@@ -8,11 +8,10 @@ import BlogPost from './components/BlogPost';
 import AddEditPost from './components/AddEditPost';
 import './styles/animations.css'; // Import CSS for animations
 
-// Main application component
 const App = () => {
   return (
     <Router>
-      <GlobalStyle /> {/* Apply global styles */}
+      <GlobalStyle />
       <Nav>
         <Title>Blogging Platform</Title>
         <NavLinks>
@@ -20,30 +19,28 @@ const App = () => {
           <NavLink to="/new">Add New Post</NavLink>
         </NavLinks>
       </Nav>
-      <AnimatedRoutes /> {/* Render animated routes */}
+      <AnimatedRoutes />
     </Router>
   );
 };
 
-// Component to handle route transitions with animations
 const AnimatedRoutes = () => {
-  const location = useLocation(); // Get the current location
+  const location = useLocation();
 
   return (
     <TransitionGroup>
       <CSSTransition key={location.key} classNames="fade" timeout={300}>
         <Routes location={location}>
-          <Route path="/" element={<BlogPostList />} /> {/* Route for the blog post list */}
-          <Route path="/post/:id" element={<BlogPost />} /> {/* Route for viewing a single blog post */}
-          <Route path="/edit/:id" element={<AddEditPost />} /> {/* Route for editing a blog post */}
-          <Route path="/new" element={<AddEditPost />} /> {/* Route for adding a new blog post */}
+          <Route path="/" element={<BlogPostList />} />
+          <Route path="/post/:id" element={<BlogPost />} />
+          <Route path="/edit/:id" element={<AddEditPost />} />
+          <Route path="/new" element={<AddEditPost />} />
         </Routes>
       </CSSTransition>
     </TransitionGroup>
   );
 };
 
-// Styled component for the navigation bar
 const Nav = styled.nav`
   background: #333;
   color: #fff;
@@ -52,18 +49,15 @@ const Nav = styled.nav`
   padding: 1rem;
 `;
 
-// Styled component for the site title
 const Title = styled.h1`
   margin: 0;
 `;
 
-// Styled component for the navigation links container
 const NavLinks = styled.div`
   display: flex;
   gap: 1rem;
 `;
 
-// Styled component for each navigation link
 const NavLink = styled(Link)`
   color: #fff;
   text-decoration: none;
